9096015d895251020c8774140b3a5d3ed82e6f5d
emmanuelm41 <emmanuelm41@gmail.com>
2024-08-30 15:04:58 -0300

    feat: add zemu log fns

--- src/utils.rs
+++ src/utils.rs
@@ -40,3 +40,26 @@ impl TryFrom<&[u8]> for Bip32Path {
         ))
     }
 }
+
+extern "C"{
+    fn check_app_canary();
+    fn zemu_log_stack(ctx: *const u8);
+    fn zemu_log(buf: *const u8);
+}
+
+pub fn z_check_app_canary(){
+    unsafe{
+        check_app_canary()
+    }
+}
+
+pub fn zlog(buf: &str){
+    unsafe{
+        zemu_log(buf.as_bytes().as_ptr())
+    }
+}
+pub fn zlog_stack(buf: &str){
+    unsafe{
+        zemu_log_stack(buf.as_bytes().as_ptr())
+    }
+}
